589ab74c3ccce4fff4bcaca57a8b94956db7e397
abhayrajsingh099 <abhayrajsingh2091@gmail.com>
2026-01-11 10:15:45 +0530

    Added PostgreSQL Docker

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator functions
+"""
+
+def add(x, y):
+    """ADD x and y and return result."""
+    return x+y
+
+def subtract(x, y):
+    """Subtract x and y and return result."""
+    return y - x
